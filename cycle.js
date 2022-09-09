1d7dfeb05cdf78011c6dad0f173404c658d72f21
Interchained <president@worldvaporexpo.com>
2022-09-09 14:38:03 -0400

    kekpoint: snapshot ERC20 balances

---
+++ cycle.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('balances/KEK.json');
+let holders = JSON.parse(rawdata);
+// console.log(holders);
+for(var i = 0; i < holders.length; i++)
+{
+    var wallets = holders[i].wallet;
+    var amounts = holders[i].balance;
+    console.log(wallets);
+    console.log(amounts);
+}
